6661e5e832e8ab793381880f53cb4d0b94e9820d
MikhailChuza <148324632+MikhailChuza@users.noreply.github.com>
2024-01-28 15:25:14 +0200

    task terminal

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,20 @@
-function filterArray(numbers, value) {
-  let filteredArray = [];
-   for (let i = 0; i < numbers.length; i++) {
-   if (numbers[i] > value) {
+// function filterArray(numbers, value) {
+//   let filteredArray = [];
+//    for (let i = 0; i < numbers.length; i++) {
+//    if (numbers[i] > value) {
      
-      filteredArray.push(numbers[i]);
-    }
-  }
- return filteredArray;
-}
-// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+//       filteredArray.push(numbers[i]);
+//     }
+//   }
+//  return filteredArray;
+// }
+// // console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// // console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// // console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// // console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// // console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+const name = prompt("enter your name");
+const email = prompt("enter your email");
+const tel = prompt("enter your tel");
+alert(`користувач ${name} використовує пошту ${email} телефон ${tel}`);
